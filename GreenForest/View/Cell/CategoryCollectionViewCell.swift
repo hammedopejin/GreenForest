7d8c31b7058b5117e82e305cd8c3340d25801c44
Hammed Opejin <hammedopejin@yahoo.com>
2020-02-01 19:06:18 -0500

    feat: Create CategoryCollectionViewController

---
+++ GreenForest/View/Cell/CategoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCollectionViewCell.swift
+//  GreenForest
+//
+//  Created by Hammed opejin on 2/1/20.
+//  Copyright © 2020 Hammed opejin. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+func generateCell(_ category: Category) {
+        
+    }
+}

b4794f2fc2fd3ea5a87fb63f9836d6d7a533ec88
Hammed Opejin <hammedopejin@yahoo.com>
2020-02-02 20:47:41 -0500

    feat: Create ImageCollectionViewCell

---
+++ GreenForest/View/Cell/ImageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCollectionViewCell.swift
+//  GreenForest
+//
+//  Created by Hammed opejin on 2/2/20.
+//  Copyright © 2020 Hammed opejin. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    func setupImageWith(itemImage: UIImage) {
+        
+    }
+}
